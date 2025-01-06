583b138b810b7334e332c4afa65a98ad62eade8f
赵鸿飞 <zhaohongfei@vasoyn.com>
2025-01-06 09:13:35 +0800

    fix: 调整瀑布流组件

--- pages/component/WaterfallFlow.vue
+++ pages/component/WaterfallFlow.vue
@@ -6,7 +6,21 @@
       :key="columnIndex"
     >
       <view class="waterfall-item" v-for="(item, index) in column" :key="index">
-        <slot name="item" :item="item"></slot>
+        <view class="food-card" @click="handleClickCard(item)">
+          <image :src="item.image" mode="widthFix" class="food-image" />
+          <view class="card-content">
+            <text class="title">{{ item.title }}</text>
+            <view class="user-info">
+              <view class="user-left">
+                <image :src="item.avatar" class="avatar" mode="aspectFill" />
+                <text class="username">{{ item.username }}</text>
+              </view>
+              <view class="rating">
+                <text class="rating-text">{{ item.rating.toFixed(1) }}</text>
+              </view>
+            </view>
+          </view>
+        </view>
       </view>
     </view>
   </view>
@@ -67,9 +81,20 @@ const initLayout = () => {
 onMounted(() => {
   initLayout();
 });
+const handleClickCard = (item) => {
+  uni.navigateTo({
+    url: `/pages/details/index?id=${item.id}`,
+  });
+};
 </script>
 
 <style lang="scss" scoped>
+// 文本省略混入
+@mixin text-ellipsis {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
 .waterfall-container {
   display: flex;
   padding: 10rpx;
@@ -90,6 +115,68 @@ onMounted(() => {
       &:hover {
         transform: translateY(-2rpx);
         box-shadow: 0 4rpx 16rpx rgba(0, 0, 0, 0.15);
+      }
+
+      .food-card {
+        .food-image {
+          width: 100%;
+          height: auto;
+          background-color: #f5f5f5;
+        }
+
+        .card-content {
+          padding: 16rpx;
+
+          .title {
+            font-size: 28rpx;
+            color: #333;
+            line-height: 1.4;
+            margin-bottom: 16rpx;
+            font-weight: 500;
+            display: -webkit-box;
+            -webkit-box-orient: vertical;
+            -webkit-line-clamp: 2;
+            overflow: hidden;
+          }
+
+          .user-info {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+
+            .user-left {
+              display: flex;
+              align-items: center;
+              flex: 1;
+              min-width: 0; // 防止子元素溢出
+
+              .avatar {
+                width: 40rpx;
+                height: 40rpx;
+                border-radius: 50%;
+                margin-right: 12rpx;
+              }
+
+              .username {
+                font-size: 24rpx;
+                color: #666;
+                @include text-ellipsis;
+              }
+            }
+
+            .rating {
+              display: flex;
+              align-items: center;
+
+              .rating-text {
+                margin-left: 8rpx;
+                font-size: 24rpx;
+                color: #ffc300;
+                font-weight: 500;
+              }
+            }
+          }
+        }
       }
     }
   }
