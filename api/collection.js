a2815a95b61e995ea176911061b583498341b339
赵鸿飞 <zhaohongfei@vasoyn.com>
2025-01-15 14:00:47 +0800

    feat: 完成收藏列表功能

--- api/collection.js
+++ api/collection.js
@@ -1,8 +1,14 @@
-import { post } from "@/utils/request";
+import { post, get } from "@/utils/request";
 
 export function createCollection(data) {
   return post({
     url: `/collection`,
     data,
+  });
+}
+
+export function collectionList() {
+  return get({
+    url: `/collection`,
   });
 }
--- pages/user/myCollection/index.vue
+++ pages/user/myCollection/index.vue
@@ -13,8 +13,8 @@
     <!-- 收藏列表 -->
     <view class="collection-list">
       <WaterfallFlow
-        v-if="collectionList.length > 0"
-        :list="collectionList"
+        v-if="list.length > 0"
+        :list="list"
         :column-count="2"
         :column-gap="30"
       >
@@ -32,37 +32,18 @@
 <script setup>
 import { ref } from "vue";
 import WaterfallFlow from "@/pages/component/WaterfallFlow.vue";
+import { collectionList } from "@/api/collection";
+import { onShow } from "@dcloudio/uni-app";
 
 // 收藏列表数据
-const collectionList = ref([
-  {
-    title: "超级好吃的日式拉面，汤底浓郁，面条劲道",
-    image:
-      "https://images.unsplash.com/photo-1569718212165-3a8278d5f624?ixlib=rb-4.0.3",
-    avatar:
-      "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&w=50&h=50",
-    username: "美食达人",
-    rating: 4.8,
-  },
-  {
-    title: "探店 | 这家藏在巷子里的小店太惊艳了",
-    image:
-      "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-4.0.3",
-    avatar:
-      "https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-4.0.3&w=50&h=50",
-    username: "吃货小王",
-    rating: 4.5,
-  },
-  {
-    title: "重庆火锅太香了！麻辣鲜香，让人欲罢不能",
-    image:
-      "https://images.unsplash.com/photo-1569058242567-93de6f36f8eb?ixlib=rb-4.0.3",
-    avatar:
-      "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&w=50&h=50",
-    username: "火锅控",
-    rating: 4.9,
-  },
-]);
+const list = ref([]);
+
+onShow(async () => {
+  const res = await collectionList();
+  res.forEach((item) => {
+    list.value.push(item.food);
+  });
+});
 
 /**
  * 返回上一页
